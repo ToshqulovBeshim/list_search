bf93a2ad1aa358fc340029775b968f9e7af6faec
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-09 17:52:06 +0300

    find8

--- find08_min_count.py
+++ find08_min_count.py
@@ -1,8 +1,15 @@
-def find_min_count(data):
+def find_min_count(d):
     """
     Given the list of numbers, Find count of minimum numbers in the list
     args:
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    a=0
+    mi=d[0]
+    while a<len(d):
+        if mi<=d[a]:
+            mi=d[a]
+        a+=1
+    return d.count(mi) 
+print (find_min_count([3,4,2,5,6,7,3]))
