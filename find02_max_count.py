e96258b5a0e93784b0458129f551c6e2c6a5ca6e
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-09 17:59:39 +0300

    allamurod dalbayop 2

--- find02_max_count.py
+++ find02_max_count.py
@@ -1,8 +1,17 @@
-def find_max_count(data):
+def find_max_count(d):
     """
     Given the list of numbers, Find count of maximum numbers in the list
     args:
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    a=0
+    ma=d[0]
+    while a<len(d):
+        if ma<=d[a]:
+            ma=d[a]
+        a+=1
+    
+    
+    return d.count(ma)
+print (find_max_count([2,4,6,76543,76543,76543]))
