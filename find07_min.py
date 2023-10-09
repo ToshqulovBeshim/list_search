1d7c32d3ee63ae76d690cbf8ef23d9fb912dbdfe
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-09 17:51:41 +0300

    find7

--- find07_min.py
+++ find07_min.py
@@ -1,9 +1,15 @@
-def find_min(data):
+def find_min(d):
     """
     Given the list of numbers, return the minimum number in the list
     args:
         data: list of numbers
     returns: minimum number in the list
     """
-
-    return 0
+    a=0
+    mi=d[0]
+    while a<len(d):
+        if mi>=d[a]:
+            mi=d[a]
+        a+=1
+    return mi
+print (find_min ([3,4,5,8,2]))
