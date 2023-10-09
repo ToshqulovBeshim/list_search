ad4eba7d7ccc56ffca32af3f83f5c6531936c5ba
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-09 17:57:08 +0300

    find loop 1

--- find01_max.py
+++ find01_max.py
@@ -1,9 +1,16 @@
-def find_max(data):
+def find_max(d):
     """
     Given the list of numbers, return the maximum number in the list
     args:
         data: list of numbers
     returns: maximum number in the list
     """
-    return 0
+    a=0
+    ma=d[0]
+    while a<len(d):
+        if ma<=d[a]:
+            ma=d[a]
+        a+=1
+    return ma
+print(find_max([1,3,4,2,7,5]))
     
