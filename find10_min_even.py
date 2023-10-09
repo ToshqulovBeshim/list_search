db0f5b2e9c5b78aae70faea93e6fab721e49257f
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-09 17:53:10 +0300

    find10

--- find10_min_even.py
+++ find10_min_even.py
@@ -1,9 +1,16 @@
-def find_min_even(data):
+def find_min_even(d):
     """
     Given the list of numbers, Find the minimum even number in the list
     args:
         data: list of numbers
     returns: minimum even number in the list
     """
-    return 0
+    a=0
+    mi=d[0]
+    while a<len(d):
+        if mi<=d[a] and d[a]%2==0:
+            mi=d[a]
+        a+=1
+    return mi
+print(find_min_even([67,4567,345,3456,456789]))
 
