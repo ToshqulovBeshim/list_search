00a6a4b32b819f9191339eb472ab2785eadb48b1
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-09 17:52:32 +0300

    find8

--- find09_min_index.py
+++ find09_min_index.py
@@ -1,9 +1,17 @@
-def find_min_index(data):
+def find_min_index(d):
     """
     Given the list of numbers, return the index of minimum number in the list
     args:
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
+    a=0
+    mi=d[0]
+    while a<len(d):
+        if mi<=d[a]:
+            mi=d[a]
+            b=a
+        a+=1
+    return b+1
+print (find_min_index([3,45,6,7,65,4,5,6]))
 
