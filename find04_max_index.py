23028f74407224abe9056d6cdb56e6c88c34a383
ToshqulovBeshim <tbeshimboyy@gmail.com>
2023-10-09 17:49:46 +0300

    fint2

--- find04_max_index.py
+++ find04_max_index.py
@@ -1,8 +1,16 @@
-def find_max_index(data):
+def find_max_index(d):
     """
     Given the list of numbers, return the index of maximum number in the list
     args:
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    a=0
+    ma=d[0]
+    while a<len(d):
+        if ma<=d[a]:
+            ma=d[a]
+            b=a
+        a+=1
+    return b+1
+print (find_max_index([2,3,5,6,7,4,2,2,3,4,6,6]))
